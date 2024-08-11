67eee5830a35a646718a4c65dcaf413631023ec8
CanBG <cansarper1@hotmail.com>
2024-08-11 12:22:19 +0300

    Add files via upload

--- websites/parkdetails.php
+++ websites/parkdetails.php
@@ -2,12 +2,39 @@
     session_start();
     if($_SESSION["validate"] == 2){
         include("parkdetails.html");
-        
-
+        include("database2.php"); 
 
         $id = $_GET["id"];
-        echo $id;
-        echo "Not Done";
+        
+        $query = "SELECT * FROM parks WHERE id = $id ";
+        $result = $conn2->query($query);
+        $row = $result->fetch_assoc();
+
+        
+        $ratio = floor($row["occupied"] * (360/$row["slots"]));
+        $deg = $ratio . "deg";
+        
+        echo 
+        "
+        <div class = 'container'>
+            <br>
+            <br>
+            <h2> $row[name] Park </h2>
+            <style>
+                .piechart{
+                    background-image: conic-gradient(
+                        blue $deg,
+                        cyan 0deg
+                    );
+             }
+            </style>
+            <br>
+            <div class='piechart'></div>
+            <br>
+            <h4> Availability $row[occupied]/$row[slots] </h4>
+        </div>
+
+        ";
         
 
     
